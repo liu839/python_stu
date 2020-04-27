99e673ebfd597113f70d9dbde36b73a0d28ecb1a
liu839 <710374839@qq.com>
2020-04-27 23:05:03 +0800

    我太难了

--- 程序记录/乐扣/Z字变换.py
+++ 程序记录/乐扣/Z字变换.py
@@ -30,3 +30,17 @@ def convert(self, s: str, numRows: int) -> str:
             str1+=''.join(matrix[i])
         str1=str1.replace(" ","")
         return str1
+
+"""
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows < 2: return s
+        res = ["" for _ in range(numRows)]
+        i, flag = 0, -1
+        for c in s:
+            res[i] += c
+            if i == 0 or i == numRows - 1: flag = -flag
+            i += flag
+        return "".join(res)
+
+"""
