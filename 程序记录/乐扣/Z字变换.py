f24435b0f04447a6c7c169ea59345d467f20ff25
liu839 <710374839@qq.com>
2020-04-27 22:52:18 +0800

    刷题刷题

---
+++ 程序记录/乐扣/Z字变换.py
@@ -0,0 +1,32 @@
+
+def convert(self, s: str, numRows: int) -> str:
+    if len(s)==0 or len(s)==1 or numRows==1:
+            return s
+        matrix=[[' ']for _ in range(numRows)]
+        matrix[0][0]=s[0]
+        i=1
+        j=0
+        num=1
+        x=1
+        while True:    
+            matrix[i][j]=s[num]
+            if x==-1 and i!=0:
+                j+=1
+                for index in range(numRows):
+                    matrix[index].append(' ')
+            if i+1==numRows:
+                x=-x
+                j+=1
+                for index in range(numRows):
+                    matrix[index].append(' ')
+            elif i==0:
+                x=-x
+            i=i+x*1
+            num+=1
+            if num==len(s):
+                break
+        str1=''
+        for i in range(numRows):
+            str1+=''.join(matrix[i])
+        str1=str1.replace(" ","")
+        return str1
--- 草稿.py
+++
@@ -1,5 +0,0 @@
-i=6
-print(i*3)
-print('liu')
-print(33)
-print(44)
