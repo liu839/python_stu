519da79b2d30b1bc73ce58794db9cc0bb8636d8b
liu839 <710374839@qq.com>
2020-04-28 14:09:32 +0800

    针对优化

--- 程序记录/excel/对比转录优化版.py
+++ 程序记录/excel/对比转录优化版.py
@@ -14,38 +14,30 @@ list_dirname=os.listdir(dirname)
 
 for eachname in list_dirname:
     wb2=openpyxl.load_workbook(eachname)
-    ws2=wb2["学生成绩详情表"]
-
+    ws2=wb2["Sheet1"]
+    print("正在判断%s"%(eachname))
     for each in ws1['A1':'L1']:
         #识别目前在判断什么成绩
         for i in each:
             if i.value in eachname:
                 score_1_col=dict_[i.value]
+                score_2_col=score_1_col
                 break
-
-    i=0#临时存储总分的col
-    for each in ws2['A1':'L1']:
-        #寻找总分位置
-        for temp in each:
-            if '总分'in temp.value:
-                score_2_col=list_[i]
-                break
-            i+=1
-
 
     for i_1 in range(2,ws1.max_row+1):
     #实际转录模块
         name_1_sign='C'+str(i_1)
         name_1=ws1[name_1_sign].value
         score_1_sign=score_1_col+str(i_1)
-        #score_1=ws1[score_1_sign].value   测试
+        score_1=ws1[score_1_sign].value   
         for i_2 in range(2,ws2.max_row+1):
-            name_2_sign='A'+str(i_2)
+            name_2_sign='C'+str(i_2)
             name_2=ws2[name_2_sign].value
             score_2_sign=score_2_col+str(i_2)
             score_2=ws2[score_2_sign].value
             if name_1==name_2:
                 ws1[score_1_sign].value=ws2[score_2_sign].value
+                break
 
 
-wb1.save(r'C:\Users\71037\Desktop\完成.xlsx')
+wb1.save(r'C:\Users\71037\Desktop\总表.xlsx')
--- 程序记录/乐扣/读取字符串中数字.py
+++ 程序记录/乐扣/读取字符串中数字.py
@@ -1,3 +1,3 @@
 import re
-def myAtoi(self, str: str) -> int:
-    return max(min(int(*re.findall('^[\+\-]?\d+', str.lstrip())), 2**31 - 1), -2**31)
+def myAtoi(str):
+    return max(min(int(*re.findall(r'^[\+\-]?\d+', str.lstrip())), 2**31 - 1), -2**31)
