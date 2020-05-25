41aff8ed04bafe63526972f164886c0cf067c16d
liu839 <710374839@qq.com>
2020-05-25 20:26:31 +0800

    学习

---
+++ 程序记录/力扣/20有效括号.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {'{': '}',  '[': ']', '(': ')', '?': '?'}
+        stack = ['?']
+        for c in s:
+            if c in dic: stack.append(c)
+            elif dic[stack.pop()] != c: return False 
+        return len(stack) == 1
+
