463e35f54cbfacf67af73547d0e787ff23fc4af1
liu839 <710374839@qq.com>
2020-06-13 22:28:54 +0800

    尝试与gitee链接2

--- 程序记录/爬虫/plmm.py
+++ 程序记录/爬虫/plmm.py
@@ -8,6 +8,7 @@ import re
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.25 Safari/537.36 Core/1.70.3741.400 QQBrowser/10.5.3863.400'}
 url_low='http://jandan.net/ooxx'
 jpg_count=0
+
 def jpg_download(url):
     #图片下载区域
     global jpg_count
