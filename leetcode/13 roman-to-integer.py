b0897c9ff27a3b384636d5e63caa5c90a8ac129f
liu839 <710374839@qq.com>
2020-07-18 16:09:44 +0800

    答题修正

--- leetcode/13 roman-to-integer.py
+++ leetcode/13 roman-to-integer.py
@@ -1,11 +1,24 @@
 def a(s):
-    dict_={'CM':900,'CD':400,'XC':90,'XL':40,'IX':9,'V':5,'IX':4,'I':1,'M':1000,'D':500,'C':100,'L':50,'X':10}
+    dict_change={'CM':900,'CD':400,'XC':90,'XL':40,'IX':9,'IV':4,'I':1,'V':5,'M':1000,'D':500,'C':100,'L':50,'X':10}
     sum_=0
-    for each in dict_:
+    for each in dict_change:
         if not len(s):
             break
         while each in s:
             s=s.replace(each,"",1)
-            sum_+=dict_[each]
+            sum_+=dict_change[each]
     return sum_
-print(a("III"))
+
+def b(s):
+    dict_change={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    res=0
+    try:
+        for index,each in enumerate(s):
+            if dict_change[each] < dict_change[s[index + 1]]:
+                res -= dict_change[each]
+            else:
+                res += dict_change[each]
+    except IndexError:
+        res += dict_change[s[-1]]
+    return res
+print(b("LVIII"))
