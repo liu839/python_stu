43f24b4967bfbd1a145bb01083f241d0684bfcfa
liu839 <710374839@qq.com>
2020-08-13 14:45:37 +0800

    优化

--- leetcode/55jump-game.py
+++ leetcode/55jump-game.py
@@ -4,4 +4,6 @@ class Solution:
         for i, jump in enumerate(nums):   #i为当前位置，jump是当前位置的跳数
             if max_i>=i and i+jump>max_i:  #如果当前位置能到达，并且当前位置+跳数>最远位置  
                 max_i = i+jump  #更新最远能到达位置
+            if i > max_i:
+                return False
         return max_i>=i
