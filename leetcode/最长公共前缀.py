5665439f371aa8ab5193010c55dff5d3f5f9f421
liu839 <710374839@qq.com>
2020-09-14 10:57:34 +0800

    常规优化

--- leetcode/最长公共前缀.py
+++ leetcode/最长公共前缀.py
@@ -1,24 +1,24 @@
 def x(strs):
     #        return os.path.commonprefix(strs) 
     l=len(strs)
-    if l==0 or ("" in strs):
+    if l == 0 or ("" in strs):
         return ""
-    elif l==1:
+    elif l == 1:
         return strs[0]
-    str_=''
-    i=j=0
+    str_ = ''
+    i = j = 0
     while True:
         for index in range(l-1):
             try:
                 if strs[index][i]!=strs[index+1][i]:
                     break
-                j+=1
+                j += 1
             except IndexError:
                 return str_
-        if j==l-1:
+        if j == l-1:
             str_+=strs[index][i]
-            i+=1
-            j=0
+            i += 1
+            j = 0
         else:
             return str_
 print(x(["flower","flow","flight"]))
