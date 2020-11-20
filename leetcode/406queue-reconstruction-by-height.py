d00ac7505c05c1bd1f4b8587b7b5fe42394c1f30
liu839 <710374839@qq.com>
2020-11-20 19:23:43 +0800

    11/20做题

---
+++ leetcode/406queue-reconstruction-by-height.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        res = []
+        people = sorted(people, key = lambda x: (-x[0], x[1]))
+        print(people)
+        for p in people:
+            if len(res) <= p[1]:
+                res.append(p)
+            elif len(res) > p[1]:
+                res.insert(p[1], p)
+        return res
