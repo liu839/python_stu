99f02965986c22f3935d738363ff82af41bdf372
unknown <710374839@qq.com>
2020-12-12 16:55:38 +0800

    修改计网

--- 计网/clinet.py
+++ 计网/clinet.py
@@ -1,19 +1,31 @@
-
+'''创建客户端'''
 import socket
+import base64
 
-print("测试中===========")
-
-s = socket.socket()
-
-host = socket.gethostname()
-print(host)
-port = 63214
-s.connect((host, port))
-
-while True:
+def reve():
+    print("收到的服务的消息：")
     res = s.recv(1024).decode()
-    if res == " ":
-        break
-    print("收到内容%s" % res)
+    print(res)
+    decode = base64.b64decode(res)
+    decode = ''.join(chr(each) for each in decode.split(" "))
+    print(decode.decode("utf8"))
 
-s.close()
+def send():
+    sends = input("客户端发送的消息：")
+    if sends == 'qq':
+        raise SystemError
+    sends = s.send(base64.b64encode(sends.encode()))
+
+print("连接中===========")
+
+host = '192.168.43.21'
+port = 63214
+s = socket.socket()
+s.connect_ex((host, port))
+print("已建立链接！")
+try:
+    while True:
+        reve()
+        send()
+except SystemError:
+    s.close()
